47a6804f625239a8e0b1afa9cb8368f395256699
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-04-18 23:40:16 +0100

    completed the featured section

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,8 +1,29 @@
+import { featuredProducts } from "@/data"
+import Image from "next/image"
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className="w-screen overflow-x-scroll text-blue-500">
+      <div className="w-max flex">
+        {
+          featuredProducts.map((item) => (
+            <div key={item.id} className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-gray-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[80vh]">
+              <div className="flex-1 hover:rotate-[60deg] transition-all duration-500">
+                <Image src={item.img} alt="dumplings image" height={200} width={200} className="object-contain rounded-full" />
+              </div>
+
+              <div className="flex-1 flex flex-col gap-4 items-center justify-center text-center">
+                <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
+                <p className="p-4 2xl:p-8">{item.desc}</p>
+                <span className="text-xl font-bold">${item.price}</span>
+                <button className="bg-blue-500 text-white p-2 rounded-md">Add to cart</button>
+              </div>
+          </div>
+          ))
+        }
+      </div>
+    </div>
   )
-}
+} 
 
 export default Featured
---
+++ data.jsx
@@ -0,0 +1,192 @@
+export const featuredProducts = [
+    {
+        id: 1,
+        title: "Barcelona",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio voluptates reprehenderit pariatur rerum aperiam cum libero fugiat provident nihil sapiente?",
+        img: "/images/schnitzel.jpg",
+        price: "23.4",
+        options: [
+            {
+                title: "small",
+                additionalPrice: 0,
+            },
+            {
+                title: "medium",
+                additionalPrice: 4,
+            },
+            {
+                title: "large",
+                additionalPrice: 6,
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "Juventus",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio voluptates reprehenderit pariatur rerum aperiam cum libero fugiat provident nihil sapiente?",
+        img: "/images/tomato-salad.jpg",
+        price: "27.3",
+        options: [
+            {
+                title: "small",
+                additionalPrice: 0,
+            },
+            {
+                title: "medium",
+                additionalPrice: 4,
+            },
+            {
+                title: "large",
+                additionalPrice: 6,
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "Chelsea",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio voluptates reprehenderit pariatur rerum aperiam cum libero fugiat provident nihil sapiente?",
+        img: "/images/pizza.jpg",
+        price: "25.5",
+        options: [
+            {
+                title: "small",
+                additionalPrice: 0,
+            },
+            {
+                title: "medium",
+                additionalPrice: 4,
+            },
+            {
+                title: "large",
+                additionalPrice: 6,
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "Leverkusen",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio voluptates reprehenderit pariatur rerum aperiam cum libero fugiat provident nihil sapiente?",
+        img: "/images/macncheese.jpg",
+        price: "30.5",
+        options: [
+            {
+                title: "small",
+                additionalPrice: 0,
+            },
+            {
+                title: "medium",
+                additionalPrice: 4,
+            },
+            {
+                title: "large",
+                additionalPrice: 6,
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "Porto",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio voluptates reprehenderit pariatur rerum aperiam cum libero fugiat provident nihil sapiente?",
+        img: "/images/dumplings.jpg",
+        price: "22.5",
+        options: [
+            {
+                title: "small",
+                additionalPrice: 0,
+            },
+            {
+                title: "medium",
+                additionalPrice: 4,
+            },
+            {
+                title: "large",
+                additionalPrice: 6,
+            },
+        ],
+    },
+    {
+        id: 6,
+        title: "Paris",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio voluptates reprehenderit pariatur rerum aperiam cum libero fugiat provident nihil sapiente?",
+        img: "/images/curry.jpg",
+        price: "28.1",
+        options: [
+            {
+                title: "small",
+                additionalPrice: 0,
+            },
+            {
+                title: "medium",
+                additionalPrice: 4,
+            },
+            {
+                title: "large",
+                additionalPrice: 6,
+            },
+        ],
+    },
+    {
+        id: 7,
+        title: "Sevilla",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio voluptates reprehenderit pariatur rerum aperiam cum libero fugiat provident nihil sapiente?",
+        img: "/images/schnitzel.jpg",
+        price: "24.6",
+        options: [
+            {
+                title: "small",
+                additionalPrice: 0,
+            },
+            {
+                title: "medium",
+                additionalPrice: 4,
+            },
+            {
+                title: "large",
+                additionalPrice: 6,
+            },
+        ],
+    },
+    {
+        id: 8,
+        title: "Tottenham",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio voluptates reprehenderit pariatur rerum aperiam cum libero fugiat provident nihil sapiente?",
+        img: "/images/tomato-salad.jpg",
+        price: "26.6",
+        options: [
+            {
+                title: "small",
+                additionalPrice: 0,
+            },
+            {
+                title: "medium",
+                additionalPrice: 4,
+            },
+            {
+                title: "large",
+                additionalPrice: 6,
+            },
+        ],
+    },
+    {
+        id: 9,
+        title: "Wolfburg",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio voluptates reprehenderit pariatur rerum aperiam cum libero fugiat provident nihil sapiente?",
+        img: "/images/macncheese.jpg",
+        price: "24.9",
+        options: [
+            {
+                title: "small",
+                additionalPrice: 0,
+            },
+            {
+                title: "medium",
+                additionalPrice: 4,
+            },
+            {
+                title: "large",
+                additionalPrice: 6,
+            },
+        ],
+    },
+]
+
